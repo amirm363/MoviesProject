fb123bc4adbcb65b933e730660977102414f5f6d
amir-PZProjects <amir@projects.org.il>
2023-05-19 08:17:54 +0300

    added data to modal

--- reactmoviesproj/src/cmps/MovieModal/MovieModal.cmp.tsx
+++ reactmoviesproj/src/cmps/MovieModal/MovieModal.cmp.tsx
@@ -10,8 +10,21 @@ interface MovieModalProps {
 export default function MovieModal({ closeModal, movieData }: MovieModalProps) {
     return (
         <div className={Style.MovieModalBackGround} onClick={closeModal}>
-            <div className={Style.MovieModalMainContainer} onClick={(ev: React.MouseEvent) => ev.stopPropagation()}>
-                <img src={movieData.image["original"]} style={{ height: "400px", width: "350px" }} />
+            <div className={`${Style.MovieModalMainContainer} animate__animated animate__zoomIn animate__faster`} onClick={(ev: React.MouseEvent) => ev.stopPropagation()}>
+                <img src={movieData.image["original"]} />
+                <div className={Style.MovieDataDiv}>
+                    <div className={Style.MoviesDataFirstRow}>
+                        <span>Name:</span>
+                        <span>Language:</span>
+                        <span>Genres:</span>
+                    </div>
+                    <div className={Style.MoviesDataSecondRow}>
+                        <span>{movieData.name}</span>
+                        <span>{movieData.language}</span>
+                        <span>{movieData.genres.toString()}</span>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
